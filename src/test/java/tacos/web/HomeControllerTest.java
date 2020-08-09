b2cf53cfdc6134040cabd0aa551e372f87adce62
tokovach <t.kovachev1996@gmail.com>
2020-08-09 19:42:21 +0300

    Added new functionality to design taco.

--- src/test/java/tacos/web/HomeControllerTest.java
+++ src/test/java/tacos/web/HomeControllerTest.java
@@ -6,25 +6,22 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(HomeController.class)
 class HomeControllerTest {
 
-    @Autowired
-    private MockMvc mockMvc;
+	@Autowired
+	private MockMvc mockMvc;
 
-    @Test
-    public void testHomePage() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/")).
-                andExpect(MockMvcResultMatchers.status().isOk()).
-                andExpect(MockMvcResultMatchers.view().name("home")).
-                andExpect(MockMvcResultMatchers.content().string(Matchers.containsString("Welcome to...")));
-    }
+	@Test
+	public void testHomePage() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.get("/")).
+						andExpect(MockMvcResultMatchers.status().isOk()).
+						andExpect(MockMvcResultMatchers.view().name("home")).
+						andExpect(MockMvcResultMatchers.content().string(Matchers.containsString("Welcome to...")));
+	}
 }
