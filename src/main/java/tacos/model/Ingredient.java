0922992e2010c5a518021b2281e35ea383249e41
tokovach <t.kovachev1996@gmail.com>
2020-09-11 15:01:28 +0300

    Added new functionality to design taco.

--- src/main/java/tacos/model/Ingredient.java
+++ src/main/java/tacos/model/Ingredient.java
@@ -1,21 +1,30 @@
 package tacos.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import lombok.AccessLevel;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 
 @Data
+@NoArgsConstructor(access = AccessLevel.PRIVATE, force = true)
 @RequiredArgsConstructor
+@Entity
 public class Ingredient {
 
-	private final String id;
-	private final String name;
-	private final Type type;
+  @Id private final String id;
 
-	public static enum Type {
-		WRAP,
-		PROTEIN,
-		VEGGIES,
-		CHEESE,
-		SAUCE
-	}
+  private final String name;
+
+  private final Type type;
+
+  public enum Type {
+    WRAP,
+    PROTEIN,
+    VEGGIES,
+    CHEESE,
+    SAUCE
+  }
 }
--- src/main/java/tacos/model/Taco.java
+++ src/main/java/tacos/model/Taco.java
@@ -1,15 +1,29 @@
 package tacos.model;
 
+import java.util.Date;
 import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.ManyToMany;
+import javax.persistence.PrePersist;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
+import javax.persistence.Id;
 
 import lombok.Data;
 
 @Data
+@Entity
 public class Taco {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private long id;
+
+  private Date createdAt;
 
   @NotNull
   @Pattern(
@@ -17,6 +31,12 @@ public class Taco {
       message = "Name must be at least 5 chars long and not include white spaces.")
   private String name;
 
+  @ManyToMany(targetEntity = Ingredient.class)
   @Size(min = 1, message = "You must choose at least 1 ingredient.")
   private List<String> ingredients;
+
+  @PrePersist
+  void createdAt() {
+    this.createdAt = new Date();
+  }
 }
